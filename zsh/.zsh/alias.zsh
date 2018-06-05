f7d55f7014d8328aac6cb14fa23a2cda55a40d58
Rob McFadzean <root@sphericalcube.net>
2018-06-05 14:16:18 +0930

    pacaur aliases -> trizen aliases

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -10,5 +10,5 @@ alias vi="vim"
 alias emacs="vim"
 alias ag="ag --color --color-line-number '0;35' --color-match '46;30' --color-path '4;36'"
 alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
-alias update="pacaur -Syu"
+alias update="trizen -Syu"
 alias systemctl="sudo systemctl"
